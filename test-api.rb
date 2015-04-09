4e87299c8c12a8b5ffb2db15c426ac206a7a34a2
Eric Bartos <djbartos93@gmail.com>
2015-04-08 22:19:25 -0400

    clean up

--- test-api.rb
+++ test-api.rb
@@ -7,190 +7,168 @@ require 'sinatra'
 
 CONFIG_FILE = 'config.yml'
 TOKEN_FILE = '.token.yaml'
-USER_FILE = 'user.yaml'
-DEVICE_FILE = 'device.yaml'
-OUTDOOR_TEMP = 'outdoor_temp.yaml'
-INDOOR_TEMP = 'indoor_temp.yaml'
-OUTDOOR_HUMID = 'outdoor_humid.yaml'
-INDOOR_HUMID = 'indoor_humid.yaml'
 
-def measure_config
-  @measure_config ||= YAML.load_file(TOKEN_FILE)
+######## API Auth #########
+#begin authentication section
+#config sets up the config.yml file
+
+def config
+  @config ||= YAML.load_file(CONFIG_FILE)['netatmo']
 end
 
-def read_config_modules
-  @read_config ||= YAML.load_file(DEVICE_FILE)['body']['modules'][0]
+#get_token makes the api call to netatmo
+
+def get_token
+  uri = URI.parse("http://api.netatmo.net/oauth2/token")
+  JSON.parse Net::HTTP.post_form(uri, {
+    'grant_type' => 'password',
+    'client_id' => config['client_id'],
+    'client_secret' => config['client_secret'],
+    'username' => config['username'],
+    'password' => config['password'],
+    'scope' => 'read_station'
+  }).body
 end
 
-def read_config_indoor
-  @read_config ||= YAML.load_file(DEVICE_FILE)['body']['devices'][0]
+#renew_token, following the API guidelines using the refresh token to get a new token when token expires.
+
+def renew_token(refresh_token)
+  uri = URI.parse("http://api.netatmo.net/oauth2/token")
+  JSON.parse Net::HTTP.post_form(uri, {
+    'grant_type' => 'refresh_token',
+    'refresh_token' => refresh_token,
+    'client_id' => config['client_id'],
+    'client_secret' => config['client_secret']}).body
 end
 
-#get user and save info to file
+#save_token will save the token to the .token.yaml file (TOKEN_FILE)
 
-def get_user
-  File.open TOKEN_FILE
-  puts "getting user info..."
-  uri = URI.parse('http://api.netatmo.net/api/getuser')
-  user_info = JSON.parse Net::HTTP.post_form(uri, {
-  'access_token' => measure_config['access_token']}).body
-  user_file = File.open(USER_FILE, 'w')
-  user_file.write YAML.dump user_info
-  user_file.flush
-  user_file.close
+def save_token(token_info)
+  @token = get_token
+  token_info['timestamp'] = Time.now
+  token_file = File.open TOKEN_FILE, 'w'
+  token_file.write YAML.dump token_info
+  token_file.flush
+  token_file.close
 end
 
+#check_token should be checking if the token is expired or not.
+#this currently fucks up when the token file isnt there
+def check_token(token_to_check)
+  if token_to_check['timestamp'] + token_to_check['expires_in'] < Time.now
+    token_to_check = renew_token token_to_check['refresh_token']
+    save_token token_to_check
+  end
+  token_to_check
+end
+
+#token is combinig everything to actually get the token, it will also refresh token when expired
+#call this each time, if the token is good it shouldnt do anything
+def token
+  begin
+    @token ||= YAML.load_file TOKEN_FILE
+  rescue Exception
+    @token = get_token
+    save_token @token
+  end
+
+  check_token @token
+end
+
+
+######## API Data #########
 #this gets all of the device info and puts it into the device.yaml file
 #this DOES NOT provide the most up to date measurements
 
 def get_device
-  File.open TOKEN_FILE
   puts "getting device info..."
+
   uri = URI.parse('http://api.netatmo.net/api/devicelist')
-  device_info = JSON.parse Net::HTTP.post_form(uri, {
-  'access_token' => measure_config['access_token']
+
+  JSON.parse Net::HTTP.post_form(uri, {
+    'access_token' => token['access_token']
   }).body
-  device_file = File.open(DEVICE_FILE, 'w')
-  device_file.write YAML.dump device_info
-  device_file.flush
-  device_file.close
 end
 
 #gets outdoor_temp
-
-def outdoor_temp
-  File.open TOKEN_FILE
+def outdoor_temp(device_id)
   puts "getting current outdoor temperature (c)..."
-  time= Time.now.utc.nsec
+
   uri = URI.parse('http://api.netatmo.net/api/getmeasure')
-  temp_out = JSON.parse Net::HTTP.post_form(uri, {
-  'access_token' => measure_config['access_token'],
-  'device_id' => read_config_modules['main_device'],
-  'module_id' => read_config_modules['_id'],
-  'date_end' => 'last',
-  'scale' => 'max',
-  'type' => 'temperature',
-  'real_time' => 'true'
-  }).body
-  temp_file = File.open(OUTDOOR_TEMP, 'w')
-  temp_file.write YAML.dump temp_out
+
+  JSON.parse(Net::HTTP.post_form(uri, {
+    'access_token' => token['access_token'],
+    'device_id' => get_device['main_device'],
+    'module_id' => get_device['modules'][device_id]['_id'],
+    'date_end' => 'last',
+    'scale' => 'max',
+    'type' => 'temperature',
+    'real_time' => 'true'
+  }).body)['body']
 end
 
 #gets indoor_temp
-
-def indoor_temp
-  File.open TOKEN_FILE
+def indoor_temp(device_id)
   puts "getting current indoor temperature (c)..."
-  time= Time.now.utc.nsec
+
   uri = URI.parse('http://api.netatmo.net/api/getmeasure')
-  temp_in = JSON.parse Net::HTTP.post_form(uri, {
-  'access_token' => measure_config['access_token'],
-  'device_id' => read_config_modules['main_device'],
-  'module_id' => read_config_indoor[['modules']],
-  'date_end' => 'last',
-  'scale' => 'max',
-  'type' => 'temperature',
-  'real_time' => 'true'
-  }).body
-  temp_file = File.open(INDOOR_TEMP, 'w')
-  temp_file.write YAML.dump temp_in
+
+  JSON.parse(Net::HTTP.post_form(uri, {
+    'access_token' => token['access_token'],
+    'device_id' => get_device['main_device'],
+    'module_id' => get_device['devices'][device_id]['_id'],
+    'date_end' => 'last',
+    'scale' => 'max',
+    'type' => 'temperature',
+    'real_time' => 'true'
+  }).body)['body']
 end
 
 #gets outdoor humidity
-
-def outdoor_humidity
-  File.open TOKEN_FILE
+def outdoor_humidity(device_id)
   puts "getting current outdoor humidity..."
-  time= Time.now.utc.nsec
+
   uri = URI.parse('http://api.netatmo.net/api/getmeasure')
-  humid_out = JSON.parse Net::HTTP.post_form(uri, {
-  'access_token' => measure_config['access_token'],
-  'device_id' => read_config_modules['main_device'],
-  'module_id' => read_config_modules['_id'],
+
+  JSON.parse(Net::HTTP.post_form(uri, {
+  'access_token' => token['access_token'],
+  'device_id' => get_device['main_device'],
+  'module_id' => get_device['modules'][device_id]['_id'],
   'date_end' => 'last',
   'scale' => 'max',
   'type' => 'humidity',
   'real_time' => 'true'
-  }).body
-  temp_file = File.open(OUTDOOR_HUMID, 'w')
-  temp_file.write YAML.dump humid_out
+  }).body)['body']
 end
 
 #gets indoor humidity
-
-def indoor_humidity
-  File.open TOKEN_FILE
+def indoor_humidity(device_id)
   puts "getting current indoor humidity..."
-  time= Time.now.utc.nsec
+
   uri = URI.parse('http://api.netatmo.net/api/getmeasure')
-  humid_in = JSON.parse Net::HTTP.post_form(uri, {
-  'access_token' => measure_config['access_token'],
-  'device_id' => read_config_modules['main_device'],
-  'module_id' => read_config_indoor[['modules']],
+
+  JSON.parse(Net::HTTP.post_form(uri, {
+  'access_token' => token['access_token'],
+  'device_id' => get_device['main_device'],
+  'module_id' => get_device['devices'][device_id]['_id'],
   'date_end' => 'last',
   'scale' => 'max',
   'type' => 'humidity',
   'real_time' => 'true'
-  }).body
-  return humid_in['body'][0]
-end
-
-def data
-  humid_inside = indoor_humidity['body'][0]
-end
-
-def temp_output
-  @temp_output ||= YAML.load_file(OUTDOOR_TEMP)['body'][0]
-  puts @temp_output['value']
-end
-
-
-def dashboard_data_out
-  File.open TOKEN_FILE
-  puts "getting dashboard data..."
-  uri = URI.parse('http://api.netatmo.net/api/devicelist')
-  dashboard = JSON.parse Net::HTTP.post_form(uri, {
-    'access_token' => measure_config['access_token']
-    }).body
-  @out = dashboard['body']['modules'][0]
-  @in = dashboard['body']['devices'][0]
-  @outdoor_data = @out["dashboard_data"]
-end
-
-def dashboard_data_in
-  File.open TOKEN_FILE
-  puts "getting dashboard data..."
-  uri = URI.parse('http://api.netatmo.net/api/devicelist')
-  dashboard = JSON.parse Net::HTTP.post_form(uri, {
-    'access_token' => measure_config['access_token']
-    }).body
-  @out = dashboard['body']['modules'][0]
-  @in = dashboard['body']['devices'][0]
-  @indoor_data = @in["dashboard_data"]
+  }).body)['body']
 end
 
 get '/' do
-  "#{@data_outside = dashboard_data_out}"
-  "#{@data_inside = dashboard_data_in}"
+  @indoor_temp = indoor_temp 0
+  @outdoor_temp = outdoor_temp 0
+  @indoor_humidity = indoor_humidity 0
+  @outdoor_humidity = outdoor_humidity 0
+
+  # Get alerts if any
+  @alerts = get_device['devices'][0]['meteo_alarms']
+
   erb :measure
 end
 
-=begin
-puts get_user
-puts get_device
-puts indoor_temp
-puts indoor_humidity
-puts outdoor_humidity
-puts outdoor_temp
-=end
-
-=begin
-uri = URI.parse('http://api.netatmo.net/api/getmeasure?')
-JSON.parse Net::HTTP.post_form(uri, {
-  'access_token' => ['access_token'],
-  'device_id' => 'DEVICE_ID_HERE',
-  'scale' => '30min',
-  'type' => 'Temperature',
-  'limit' => '1'})
-print JSON.parse response.body
-=end
+# Get main device from module at index 0
+puts get_device['modules'][0]['main_device']
