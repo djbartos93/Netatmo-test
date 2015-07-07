7438a1f4abf5dcbd729a4dcfa78e86f92473665f
djbartos93 <djbartos93@gmail.com>
2015-07-06 23:36:18 -0400

    attempting to admin

--- Dashboard.rb
+++ Dashboard.rb
@@ -223,6 +223,20 @@ def couch
 end
 
 
+
+###########Sinatra HTTP Auth###########
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+  end
+end
 ###########Web Pages#########
 
 get '/all' do
@@ -306,6 +320,12 @@ end
 get '/couch' do
   puts :couch
   print "couch"
+end
+
+get '/admin' do
+  protected!
+  erb :admin
+
 end
 
 
